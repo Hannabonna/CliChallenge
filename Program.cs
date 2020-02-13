0521b16fa3326c8ac1b9b27e96196aa1cecef1c2
Hanna Afiah <35686809+Hannabonna@users.noreply.github.com>
2020-02-13 11:59:22 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,17 +1,32 @@
-﻿using System.Collections.Generic;
-using System.Net.Mime;
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
 using McMaster.Extensions.CommandLineUtils;
 using System.Globalization;
-using System;
+using System.Linq;
 
 namespace cli_Challenge
 {
     [HelpOption("--hlp")]
     [Subcommand(
+        //no1
         typeof(UpperCase),
         typeof(LowerCase),
         typeof(Capital),
-        typeof(Obfuscator)
+        //no2
+        //no3
+        typeof(Palindrome),
+        //no4
+        typeof(Obfuscator),
+        //no5
+        typeof(Random),
+        //no6
+        typeof(IpAddress),
+        //no7
+        typeof(IpExternal)
+        //no8
+        //no9
+        //no10
     )]
     class Program
     {
@@ -41,7 +56,7 @@ namespace cli_Challenge
             }
         }
 
-        [Command(Description = "Command to capitalize string", Name= "capitalize")]
+        [Command(Description = "Command to capitalize string", Name = "capitalize")]
         class Capital
         {
             [Argument(0)]
@@ -53,7 +68,31 @@ namespace cli_Challenge
             }
         }
 
-        [Command(Description = "Command to obfuscator", Name= "obfuscator")]
+        [Command(Description = "Command to palindrome", Name = "palindrome")]
+        class Palindrome
+        {
+            [Argument(0)]
+            public string text { get; set; }
+
+            public void OnExecute(CommandLineApplication app)
+            {
+                string rev;
+                char[] word = text.ToCharArray();
+                Array.Reverse(word);
+
+                rev = new string(word);
+                bool palindrom = text.Equals(rev, StringComparison.OrdinalIgnoreCase);
+                if (palindrom == true)
+                {
+                    Console.WriteLine("Is palindrome? Yes");
+                } else
+                {
+                    Console.WriteLine("Is palindrome? No");
+                }
+            }
+        }
+
+        [Command(Description = "Command to obfuscator", Name = "obfuscator")]
         class Obfuscator
         {
             [Argument(0)]
@@ -64,59 +103,65 @@ namespace cli_Challenge
                 char[] b = text.ToCharArray();
                 List<string> c = new List<string>();
 
-                foreach(var d in c)
+                for(int d = 0 ; d < text.Length - 1; d++)
                 {
-                    c.Add($"&#{Convert.ToString(Convert.ToInt32(b))}");
+                    var e = Convert.ToInt32(text[d]);
+                    e.ToString();
+                    c.Add($"&#{e}");
                 }
 
                 Console.WriteLine(String.Join(";", c));
             }
         }
-        // class numberTwo
-        // {
-        //     public static int Arithmetic(){
 
-        //     }
-        // }
+        [Command(Description = "Command to random string", Name = "random")]
+        class Random
+        {
+            [Argument(0)]
+            public string text { get; set; }
 
-        // class numberThree
-        // {
-        //     public static Palindrome(string a){
-        //         string rev;
-        //         char[] word = a.ToCharArray();
-        //         Array.Reverse(word);
+            public void OnExecute(CommandLineApplication app)
+            {
+                int n = 32;
+                var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+                var ran = new char[n];
+                Random r = new Random();
 
-        //         rev = new string(word);
-        //         bool palindrom = a.Equals(rev, StringComparison.OrdinalIgnoreCase);
-        //         if (palindrom == true)
-        //         {
-        //             Console.WriteLine("Yes");
-        //         } else
-        //         {
-        //             Console.WriteLine("No");
-        //         }
-        //     }
-        // }
+                for (int i=0; i<ran.Length; i++){
+                    ran[i] = chars[r.Next];
+                }
 
-        // class numberFive 
-        // {
-        //     public static string Random()
-        //     {
-        //         var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
-        //         var s = new char[];
-        //         var random = new Random();
+                var result = new String(ran);
+            }
+        }
 
-        //         for (int i=0; i<s.Length; i++){
-        //             s[i] = chars[random.Next(chars.Length)];
-        //         }
+        [Command(Description = "Command to ip address", Name = "ip")]
+        class IpAddress
+        {
+            public void OnExecute(CommandLineApplication app)
+            {
+                string host = Dns.GetHostName();
+                string ip = Dns.GetHostEntry(host).AddressList[3].ToString();
 
-        //         var result = new String(s);
-        //     }
-        // }
+                Console.WriteLine(ip);
+            }
+        }
 
-        // class numberSix
-        // {
-            
-        // }
+        [Command(Description = "Command to ip external", Name = "ip-external")]
+        class IpExternal
+        {
+            public void OnExecute(CommandLineApplication app)
+            {
+                if (!System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
+                {
+                    Console.WriteLine("null");
+                }
+                IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+                Console.WriteLine(host.AddressList.FirstOrDefault(ip => ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)); 
+            }
+        }
+
+
+
     }
 }
